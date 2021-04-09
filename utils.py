d91fa5648ab072ecc12b12beeaa874b685febe48
gcode <gcode.wav@gmail.com>
2021-04-09 02:55:41 +0200

    export depth map frame only

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 from PIL import Image
 
@@ -46,8 +47,11 @@ def display_images(outputs, inputs=None, gt=None, is_colormap=True, is_rescale=T
     shape = (outputs[0].shape[0], outputs[0].shape[1], 3)
     
     all_images = []
-
+    c = 0
+    
     for i in range(outputs.shape[0]):
+        c += 1
+        
         imgs = []
         
         if isinstance(inputs, (list, tuple, np.ndarray)):
@@ -68,6 +72,17 @@ def display_images(outputs, inputs=None, gt=None, is_colormap=True, is_rescale=T
             imgs.append(plasma(rescaled)[:,:,:3])
         else:
             imgs.append(to_multichannel(outputs[i]))
+        
+        out_dir = r"/content/DenseDepth/outputs"
+        
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+
+        #im = Image.fromarray(np.uint8(imgs[0]*255))
+        im2 = Image.fromarray(np.uint8(imgs[1]*255))
+
+        #im.save("/content/DenseDepth/outputs/img_{1}_color.png".format(out_dir, c))
+        im2.save("/content/DenseDepth/outputs/img_{1}_depth.png".format(out_dir, c))
 
         img_set = np.hstack(imgs)
         all_images.append(img_set)
